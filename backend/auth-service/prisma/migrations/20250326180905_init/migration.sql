35e9a4f53afe43ba573b47b94a5314cf0b6476bc
Infrablok <168066305+sumit-styrk@users.noreply.github.com>
2025-08-17 21:19:37 +0530

    updated script

--- backend/auth-service/prisma/migrations/20250326180905_init/migration.sql
+++ backend/auth-service/prisma/migrations/20250326180905_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "User" (
     "name" TEXT,
     "googleId" TEXT NOT NULL,
     "image" TEXT,
-    "role" TEXT NOT NULL DEFAULT 'USER',
+    "role" TEXT NOT NULL DEFAULT 'VIEWER',
     "isActive" boolean NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -21,36 +21,3 @@ CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
 
 INSERT INTO "User" ("id", "email", "name", "googleId", "image", "role", "isActive", "createdAt", "updatedAt") VALUES
 ('04700a0f-e1a9-4662-88d9-bf093140d129','wadhwasumit@gmail.com', 'Sumit Wadhwa', '113138766342253835351', 'https://example.com/image.jpg', 'ADMIN', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-
-CREATE TABLE "Documents" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "fileName" TEXT NOT NULL,
-    "originalName" TEXT,
-    "mimeType" TEXT NOT NULL,
-    "size" bigint NOT NULL,
-    "filePath" TEXT NOT NULL,
-    "isIngested" boolean  DEFAULT false,
-    "uploadedById" TEXT NOT NULL ,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Documents_pkey" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "Ingestion_Jobs" (
-    "id" TEXT NOT NULL,
-    "documentIds" TEXT NOT NULL,
-    "status" TEXT,
-    "errorMessage" TEXT,
-    "metadata" TEXT,
-    "startedAt" TIMESTAMP(3) ,
-    "completedAt" TIMESTAMP(3) ,
-    "createdById" TEXT NOT NULL ,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Ingestion_Jobs_pkey" PRIMARY KEY ("id")
-);
