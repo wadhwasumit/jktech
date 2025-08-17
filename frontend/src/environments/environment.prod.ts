7091c45c27b80f132c01c81562351d8d186c4b6b
Infrablok <168066305+sumit-styrk@users.noreply.github.com>
2025-08-17 16:14:16 +0530

    upadte

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl:"/api",
     apiPrefix: "",
     GOOGLE_CLIENT_ID: '1017930346997-tifltb4soguftq2tap5a462si3c0krh6.apps.googleusercontent.com',
-    GOOGLE_CALLBACK_URL: 'http://a932b6387a3514d75aeccee5599dfed1-717689290.ap-south-1.elb.amazonaws.com:4200/auth/callback'
+    GOOGLE_CALLBACK_URL: 'http://abbc0d7a455b84feb8a0a6bd70b30542-373941177.ap-south-1.elb.amazonaws.com:4200/auth/callback'
   };
